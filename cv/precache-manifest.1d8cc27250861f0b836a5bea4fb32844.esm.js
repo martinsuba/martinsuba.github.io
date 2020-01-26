d986eeddff74888f59defe1fcd7e98cf44b78384
Martin Suba <martin.suba01@gmail.com>
2020-01-26 17:42:27 +0100

    add cv

---
+++ cv/precache-manifest.1d8cc27250861f0b836a5bea4fb32844.esm.js
@@ -0,0 +1,25 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/6a4fb73b2607c39f4966632fc3dfd135.jpg"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icon.png"
+  },
+  {
+    "revision": "f7724a0ae478b5c000ad1bc3127cf609",
+    "url": "/bundle.2d855.esm.js"
+  },
+  {
+    "revision": "e63c0c30411640182b25",
+    "url": "/bundle.f0334.css"
+  },
+  {
+    "revision": "40ffd20d1ebcf10a4d83dd5ca80aaa23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "199984c4fc679d86bb3d5cda2a5311ce",
+    "url": "/polyfills.8f4c6.esm.js"
+  }
+]);
---
+++ cv/precache-manifest.68424603b4592d84dafa5ac041ce38a7.js
@@ -0,0 +1,37 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/6a4fb73b2607c39f4966632fc3dfd135.jpg"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icon.png"
+  },
+  {
+    "revision": "e63c0c30411640182b25",
+    "url": "/bundle.e63c0.js"
+  },
+  {
+    "revision": "e63c0c30411640182b25",
+    "url": "/bundle.f0334.css"
+  },
+  {
+    "revision": "40ffd20d1ebcf10a4d83dd5ca80aaa23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea3a1def33ca0b225ddf",
+    "url": "/polyfills.ea3a1.js"
+  },
+  {
+    "revision": "13b82ed03d648451c57be206f75f0216",
+    "url": "/sw-debug.js"
+  },
+  {
+    "revision": "09778f5ca351a46c81caedd4f3c0cf0d",
+    "url": "/sw-esm.js"
+  },
+  {
+    "revision": "5bfd412a4280adb00029548e45e00342",
+    "url": "/sw.js"
+  }
+]);
